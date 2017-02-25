0d423c27b18019bbe988118b5245502f88728118
ekonom82 <ekonom82@inbox.ru>
2017-02-25 15:35:38 +0200

    created function goDisco

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,29 @@
 var ball = document.querySelector(".discoball");
 
-function goDisco() {
-	//do your disco magic here !
+// array of classes style
+var classesStyleArray = [".here", ".goes", ".our", ".awesome", ".disco", ".ball"];
+
+function getRandomElementFromArray (list) {
+    return list[Math.floor((Math.random()*list.length))];
 }
 
-setInterval(goDisco,300);
+// function for setting new random style to element .discoball
+function goDisco() {
+    console.log("start" + ball.classList.length);
+	//do your disco magic here !
+    var randomClassStyle = getRandomElementFromArray(classesStyleArray).substring(1);
+
+    // remove last class if length of list classes more then 1
+    if (ball.classList.length > 1) {
+        var lastClassOfElement = ball.classList[ball.classList.length - 1];
+
+        ball.classList.remove(lastClassOfElement)
+    }
+
+    // add random class to classList os our element .discoball
+    var newStyle = ball.classList.add(randomClassStyle);//ball.classList.add(randomClassStyle);
+
+    console.log("finish" + ball.classList);
+}
+
+setInterval(goDisco, 1000);
